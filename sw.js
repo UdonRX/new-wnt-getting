7e53dd603b529350d7db937373e0c4f994e4ef3c
UdonRX <ithenudail@gmail.com>
2026-08-21 07:43:31 +0900

    sw.js を更新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'personal-dashboard-v2-14-11';
+const CACHE_NAME = 'personal-dashboard-v2-14-12';
 const APP_SHELL = [
   '/',
   '/index.html',
@@ -10,6 +10,7 @@ const APP_SHELL = [
   '/src/styles/v2141.css',
   '/src/styles/v21410.css',
   '/src/styles/v21411.css',
+  '/src/styles/v21412.css',
   '/src/main.js',
   '/src/shared/icons.js',
   '/shared/paper-creative-keywords.js',
